bd91083c8d520982a871cfd7ea4edd6b14c0713f
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-08-19 21:20:36 -0300

    Function: different sounds when the buttons are clicked.

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-var key = document.querySelectorAll(".drum")
+var key = document.querySelectorAll(".drum");
 
-  for (var i = 0; i < key.length; i++)
-    key[i].addEventListener("click", function() {
-    alert("I got clicked")
-});
+for (var i = 0; i < key.length; i++)
+  key[i].addEventListener("click", function() {
+    var buttonInnerHtml = this.innerHTML;
+    switch (buttonInnerHtml) {
+      case "w":
+        var audio1 = new Audio("sounds/tom-1.mp3");
+        audio1.play();
+        break;
+      case "a":
+        var audio2 = new Audio("sounds/tom-2.mp3");
+        audio2.play();
+        break;
+      case "s":
+        var audio3 = new Audio("sounds/tom-3.mp3");
+        audio3.play();
+        break;
+      case "d":
+        var audio4 = new Audio("sounds/tom-1.mp3");
+        audio4.play();
+        break;
+      case "j":
+        var audioCrash = new Audio("sounds/crash.mp3");
+        audioCrash.play();
+        break;
+      case "k":
+        var audioKick = new Audio("sounds/kick-bass.mp3");
+        audioKick.play();
+        break;
+      case "l":
+        var audioSnare = new Audio("sounds/snare.mp3");
+        audioSnare.play();
+        break;
+      default:
+        console.log(buttonInnerHtml)
+
+    }
+
+  });
+
+//var audio = new Audio("sounds/tom-1.mp3");
+//audio.play();
